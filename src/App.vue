1a2decc38d3426e5f0c103dd0b60823ff415c295
Haoze Xu <elizurhz@gmail.com>
2017-03-05 15:42:57 +0800

    rebuild

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <div id="banner">
+      <img src="/src/assets/logo.png">
+    </div>
+    <div id="main">
+      <div id="timeline">
+        <timeline></timeline>
+      </div>
+      <img src="/src/assets/bg.jpg">
+    </div>
+  </div>
+</template>
+
+<script>
+import Timeline from './timeline.vue'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    Timeline
+  }
+}
+</script>
+
+<style>
+#timeline {
+  z-index: 1;
+  margin: auto; 
+  width: 30%;
+  position: absolute;
+  margin-left: 20%;
+}
+#main {
+  display: block; 
+  width: 100%;
+  height: 100%;
+  background: #fdbfcb;
+  position: relative;
+  left: 0;
+}
+#main img {
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+#banner {
+  display: block; 
+  background: #fdbfcb;
+  width: 100%;
+  top: 0;
+  left: 0;
+}
+#banner img {
+  width: 15%;
+  height: 100%;
+}
+</style>
